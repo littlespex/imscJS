e2f474d4f3b4ed2ab8933ba7341de94486cdbd0c
Casey Occhialini <1508707+littlespex@users.noreply.github.com>
2024-05-01 09:25:39 -0700

    Update Gruntfile.cjs
    Co-authored-by: Pierre-Anthony Lemieux <pal@sandflow.com>

--- Gruntfile.cjs
+++ Gruntfile.cjs
@@ -55,7 +55,7 @@ module.exports = function (grunt) {
             },
 
             lint: {
-                cmd: 'eslint src',
+                cmd: 'npx eslint src',
             },
 
             bundle: {
